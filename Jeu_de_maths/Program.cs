cdd2781135b9f65ef8be23bee2f55dfdbf7bee17
SAUZET Laurent <laurent.sauzetpro@gmail.com>
2023-07-14 17:08:24 +0200

    Ajoutez des fichiers projet.

---
+++ Jeu_de_maths/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Jeu_de_maths
+{
+    class Program
+    {
+        static void PoserQuestion()
+        {
+            int reponseInt = 0;
+            while (reponseInt == 0)
+            {
+                Console.Write("5 + 2 = ");
+                string reponse = Console.ReadLine();
+
+                try
+                {
+                    reponseInt = int.Parse(reponse)
+                }
+                catch
+                {
+                    Console.WriteLine("Erreur : vous devez entrer un nombre !");
+                }
+            }
+
+
+
+        }
+        static void Main(string[] args)
+        {
+            PoserQuestion();
+        }
+    }
+}
